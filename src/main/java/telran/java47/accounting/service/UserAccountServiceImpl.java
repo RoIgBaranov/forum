7b4e8b52f2d958bb204598070a91722f28edc3a0
RoIgBaranov <obscure200092@gmail.com>
2023-06-13 18:42:06 +0300

    accounting commit

---
+++ src/main/java/telran/java47/accounting/service/UserAccountServiceImpl.java
@@ -0,0 +1,80 @@
+package telran.java47.accounting.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import telran.java47.accounting.dao.UserAccountRepository;
+import telran.java47.accounting.dto.RolesDto;
+import telran.java47.accounting.dto.UserDto;
+import telran.java47.accounting.dto.UserEditDto;
+import telran.java47.accounting.dto.UserRegisterDto;
+import telran.java47.accounting.dto.exceptions.UserExistsException;
+import telran.java47.accounting.dto.exceptions.UserNotFoundException;
+import telran.java47.accounting.model.UserAccount;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserAccountServiceImpl implements UserAccountService {
+
+	final UserAccountRepository userAccountRepository;
+	final ModelMapper modelMapper;
+	
+	@Override
+	public UserDto register(UserRegisterDto userRegisterDto) {
+		if(userAccountRepository.existsById(userRegisterDto.getLogin())) {
+			throw new UserExistsException();
+		}
+		UserAccount userAccount = modelMapper.map(userRegisterDto, UserAccount.class);
+		userAccount.addRole("USER");
+		userAccountRepository.save(userAccount);
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public UserDto getUser(String login) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(()-> new UserNotFoundException());
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public UserDto removeUser(String login) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(()-> new UserNotFoundException());
+		userAccountRepository.delete(userAccount);
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public UserDto updateUser(String login, UserEditDto userEditDto) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(()-> new UserNotFoundException());
+		if(userEditDto.getFirstName()!=null) {
+			userAccount.setFirstName(userEditDto.getFirstName());
+		}
+		if(userEditDto.getLastName()!=null) {
+			userAccount.setLastName(userEditDto.getLastName());
+		}
+		userAccountRepository.save(userAccount);
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public RolesDto changeRolesList(String login, String role, boolean isAddRole) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(()-> new UserNotFoundException());
+		if(isAddRole) {
+			userAccount.addRole(role);
+		}else {
+			userAccount.removeRole(role);
+		}
+		userAccountRepository.save(userAccount);
+		return modelMapper.map(userAccount, RolesDto.class);
+	}
+
+	@Override
+	public void chagePassword(String login, String newPassword) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(()-> new UserNotFoundException());
+		userAccount.setPassword(newPassword);
+		userAccountRepository.save(userAccount);
+	}
+
+}
